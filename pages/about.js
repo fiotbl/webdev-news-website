63b6155ecf2467bf7dad66491e509fa8878eaa0f
Fiora <fioratanbeilun@gmail.com>
2022-06-19 17:40:28 +0800

    modified layout css

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function about() {
+    return (
+        <div>
+            <Head>
+                <title>
+                    About page
+                </title>
+            </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
